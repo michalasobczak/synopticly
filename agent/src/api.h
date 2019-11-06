106c2442c662135f371e54ebd794d3188da0575b
Michal Sobczak <michal.sobczak@comp.com.pl>
2019-11-06 10:16:11 +0100

    agent: switch to lua, added Dockerfile snippets for testing (ruby2.5.5, java/wildfly11)

--- agent/src/api.h
+++ agent/src/api.h
@@ -15,7 +15,8 @@
 #include <ctime>
 // ***** 3rd party libraries *****
 // #include "HTTPRequest.hpp"
-#include "httplib.h"
+// #include "httplib.h"
+#include <lua5.1/lua.hpp>
 #include "json.hpp"
 // ***** other *****
 #include "config.h"
@@ -25,21 +26,29 @@ using json = nlohmann::json;
 
 #ifndef SRC_API_H_
 #define SRC_API_H_
+	void print_error(lua_State* state) {
+	  const char* message = lua_tostring(state, -1);
+	  puts(message);
+	  lua_pop(state, 1);
+	}
+	void execute(std::string lua_code) {
+	  lua_State *state = luaL_newstate();
+	  luaL_openlibs(state);
+      luaL_loadstring(state, lua_code.c_str());
+      lua_pcall(state, 0, LUA_MULTRET, 0);
+      lua_close(state);
+	}
 	void send_data_to_api(std::string app_name, std::string hostname, std::string ruby_version) {
 		try {
 			std::cout << "send_data_to_api" << std::endl;
 			//std::time_t result = std::time(nullptr);
 			//std::cout << std::asctime(std::localtime(&result));
-
-			//http::Request request(URL);
-				httplib::Client cli(URL.c_str(), PORT);
-			std::string parameters = "/?app_name="+app_name+"&h="+hostname+"&rv="+ruby_version;
-			//const http::Response postResponse = request.send("GET", parameters, { HEADER });
-				auto res = cli.Get(parameters.c_str());
-			//std::cout << std::string(postResponse.body.begin(), postResponse.body.end()) << std::endl;
-				if (res && res->status == 200) {
-					std::cout << " response from API:" << res->body << std::endl;
-				}
+			std::string host(URL);
+			std::string port = std::to_string(PORT);
+			std::string parameters = "http://" + host + ":" + port + "/?app_name="+app_name+"&h="+hostname+"&rv="+ruby_version;
+			std::string tmp = std::string("local http=require('socket.http');local body,code,headers,status=http.request('") + parameters + std::string("');print(code,status,#body);");
+			std::cout << tmp << std::endl;
+			execute(tmp);
 		}
 		catch (const std::exception& e) {
 			std::cerr << "send_data_to_api: failed, error: " << e.what() << std::endl;
